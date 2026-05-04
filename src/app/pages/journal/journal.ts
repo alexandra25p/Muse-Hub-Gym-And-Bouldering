a58bacbce4c6a0a327d89518b2cc4593dca7e070
Alexandra <aleandreea006@gmail.com>
2026-05-04 15:31:18 +0300

    am facut paginile principale ale site ului nostru. momentan sunt doar fisierele standard cu cod generat la creare

---
+++ src/app/pages/journal/journal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-journal',
+  imports: [],
+  templateUrl: './journal.html',
+  styleUrl: './journal.scss',
+})
+export class Journal {}
